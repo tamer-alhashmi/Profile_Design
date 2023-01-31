883f974afa565463eef30795814d033dde715d4f
tamer-alhashmi <tam.elhashmy@gmail.com>
2023-02-01 00:28:56 +0200

    #scroll+js

---
+++ main.js
@@ -0,0 +1,10 @@
+let scrBtn = document.querySelector("scroll-Up");
+
+window.onscroll = function () {
+  if (window.scrollY >= 600) {
+    // console.log(`SCROLLING VALUE IS ${window.scrollY}`);
+    scrBtn.style.display = "block";
+  } else {
+    scrBtn.style.display = "none";
+  }
+};
