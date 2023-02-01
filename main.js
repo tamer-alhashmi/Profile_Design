2fb8b4a054fbd3a5109727433c1044104790e4f7
TAMER ALHASHMI <111964090+tamer-alhashmi@users.noreply.github.com>
2023-02-01 02:39:00 +0200

    Done With ScrollUp

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
-let scrBtn = document.querySelector(".scroll-Up");
+// Start Creating ScrollUp Button 
+let scrBtn = document.createElement("button");
+scrBtn.className = "scroll-Up";
+
+let scrIcon = document.createElement("i");
+scrIcon.style.fontSize = "18px";
+
+let sideBar = document.querySelector(".sidebar");
+
+scrBtn.style.cssText =
+  "background-color: var(--blue-alt-color); color: white; width: 30px; height: 30px; padding: 5px; border-radius: 5px; border: none; position: fixed; bottom: 20px; right: 20px; cursor: pointer; display: none; z-index: 100;";
+scrIcon.className = "fa-solid fa-chevron-up";
+scrBtn.append(scrIcon);
+sideBar.after(scrBtn);
 
 window.onscroll = function () {
   if (window.scrollY >= 600) {
-    // console.log(`SCROLLING VALUE IS ${window.scrollY}`);
+    // console.log(`Scrolling Value Is ${window.scrollY}`);
     scrBtn.style.display = "block";
   } else {
     scrBtn.style.display = "none";
   }
 };
-
 scrBtn.onclick = function () {
   window.scrollTo({
     left: 0,
@@ -16,3 +28,4 @@ scrBtn.onclick = function () {
     behavior: "smooth",
   });
 };
+// End Creating ScrollUp Button 
